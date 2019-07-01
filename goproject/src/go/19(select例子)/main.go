5675dfe2d933a7e1ad58adf8aff6f31bb656f9aa
songjiabin <1796254117@qq.com>
2019-07-01 22:36:10 +0800

    提交

--- goproject/src/go/19(select例子)/main.go
+++ goproject/src/go/19(select例子)/main.go
@@ -1,1 +1,28 @@
-package _9_select例子_
+package main
+
+import "fmt"
+
+var (
+	//定义了两个通道
+	chRead  = make(chan int, 1)
+	chWrite = make(chan int, 1)
+	y       = 12
+)
+
+func main() {
+	chRead <- 133
+	method()
+}
+
+func method() {
+	select {
+	case x := <-chRead:
+		// chRead 中没有数据，所以case x := <-chRead读不到数据，所以这个case不能执行。
+		fmt.Println("读到了数据是：", x)
+	case chWrite <- y:
+		//  writeCh是带缓冲区的通道，它里面是空的，可以写入1个数据，所以case writeCh <- y可以执行。
+		fmt.Println("写入了数据", y)
+	default:
+		fmt.Println("do what you to do")
+	}
+}
--- goproject/src/go/19(select例子)/main_吃饭，睡觉，打豆豆.go
+++ goproject/src/go/19(select例子)/main_吃饭，睡觉，打豆豆.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("333")
+
+	eatCh := eat()
+	sleep := time.NewTimer(3 * time.Second)
+
+	select {
+	case v := <-eatCh:
+		fmt.Println(v)
+	case v := <-sleep.C:
+		fmt.Println("睡觉了啦啊",v)
+	//default:
+	//	fmt.Println("回家打豆豆啦！")
+	}
+
+}
+
+func eat() chan string {
+
+	out := make(chan string)
+
+	go func() {
+		rand.Seed(time.Now().UnixNano())
+		time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+		out <- "喊你去回家吃饭啦"
+		close(out)
+	}()
+
+	return out
 }

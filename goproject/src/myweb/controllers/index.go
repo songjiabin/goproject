9e07d49e37db30653b2f262223dac3f9ea63d820
songjiabin <1796254117@qq.com>
2019-07-22 22:45:46 +0800

    提交

--- goproject/src/myweb/controllers/index.go
+++ goproject/src/myweb/controllers/index.go
@@ -5,6 +5,7 @@ import (
 	"github.com/astaxie/beego/orm"
 	"myweb/models"
 	"math"
+	"strconv"
 )
 
 type IndexController struct {
@@ -12,7 +13,10 @@ type IndexController struct {
 }
 
 //每页多少条数据
-const pageSize = 3
+const pageSize = 1
+
+//当前的页数
+var pageIndex = 1
 
 //展示index
 func (c *IndexController) ShowIndex() {
@@ -22,18 +26,23 @@ func (c *IndexController) ShowIndex() {
 
 	var acticles []models.Acticle
 
-	//查询表
-	_, e := newOrm.QueryTable("Acticle").All(&acticles)
-	if e != nil {
-		beego.Info("查询数据失败了")
-		return
-	}
-	c.Data["articles"] = acticles
-
 	//查询数据的条数
 	count, queryError := newOrm.QueryTable("Acticle").Count()
 	if queryError != nil {
 		beego.Info("查询数量错误", queryError)
+		return
+	}
+
+	pageIndex, _ = strconv.Atoi(c.GetString("pageIndex"))
+
+	start := pageSize * (pageIndex - 1) //开始的位置
+	//Limit 截取
+	//pageSize 一页显示多少
+	//start 从哪里开始查
+	_, e := newOrm.QueryTable("Acticle").Limit(pageSize, start).All(&acticles)
+
+	if e != nil {
+		beego.Info("查询数据失败了")
 		return
 	}
 
@@ -42,6 +51,9 @@ func (c *IndexController) ShowIndex() {
 	//一共多少页
 	totalPage := float64(count) / float64(pageSize)
 	c.Data["totalPage"] = math.Ceil(totalPage)
+	c.Data["pageIndex"] = pageIndex
+
+	c.Data["articles"] = acticles
 
 	c.TplName = "index.html"
 }

d34c1cce0e7884676828a07614ab800e46a1fb37
songjiabin <1796254117@qq.com>
2019-10-10 18:05:31 +0800

    tj

--- goproject/src/ginpro/main.go
+++ goproject/src/ginpro/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 //https://www.yoytang.com/go-gin-doc.html#Gin%20的介绍
+
 func main() {
-	// 初始化引擎
 	engine := gin.Default()
-	//注册一个路由和处理函数
-	engine.Any("/", func(context *gin.Context) {
-		context.String(http.StatusOK, "hello_world_songjiabin")
+	engine.POST("/uploads", func(context *gin.Context) {
+		//获取请求中文件的名字为 file
+		file, header, err := context.Request.FormFile("file")
+		name := context.DefaultPostForm("name", "基本密码")
+		log.Print(name)
+		if err != nil {
+			context.String(http.StatusBadRequest, "Bad request")
+			return
+		}
+		filename := header.Filename
+
+		fmt.Println(file, err, filename)
+		// 将传递的文件保存到本地
+		out, err := os.Create("C:/Users/songjiabin1/Desktop/" + filename)
+		defer out.Close()
+		io.Copy(out, file)
+		context.String(http.StatusOK, "upload successful")
+
 	})
-
-	engine.GET("/name", func(context *gin.Context) {
-		context.String(http.StatusOK, "get请求哦")
-	})
-
-	engine.Any("/any/good", hanlderGet)
-	engine.Run(":9217")
-
-}
-
-func hanlderGet(context *gin.Context) {
-	context.String(http.StatusOK, "请求成功200")
+	engine.Run()
 }
